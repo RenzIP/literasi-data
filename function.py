31f2ea1cd348e39e2d288b6e29ad1df44df39e6f
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-04-28 13:51:41 +0700

    add function calculator

--- function.py
+++ function.py
@@ -10,3 +10,16 @@ def check_room(age):
 print(check_room(25))  # Output: Masuk ke ruangan A
 print(check_room(15))  # Output: Masuk ke ruangan B
 print(check_room(10))  # Output: Masuk ke ruangan C
+
+
+def calc (a,b, operasi):
+    if operasi == '+':
+        return a + b
+    elif operasi == '-':
+        return a - b
+    elif operasi == '*':
+        return a * b
+    elif operasi == '/':
+        return a / b
+    else:
+        return "Operasi tidak valid"
