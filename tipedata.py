029f56f880c5d14b7d229b1bf4b238e62f3484d2
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-05-17 16:32:42 +0700

    push

---
+++ tipedata.py
@@ -0,0 +1,15 @@
+# Integer (Bilangan Bulat)
+umur = 18
+print(umur, type(umur))
+
+# Float (Bilangan Desimal)
+tinggi = 170.5
+print(tinggi, type(tinggi))
+
+# String (Teks)
+nama = "Bagas"
+print(nama, type(nama))
+
+# Boolean (True / False)
+is_mahasiswa = True
+print(is_mahasiswa, type(is_mahasiswa))
